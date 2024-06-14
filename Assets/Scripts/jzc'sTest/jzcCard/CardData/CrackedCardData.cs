6f454d18c3a3fc613d81474e08ebd928d4a02f5c
luanhrrr <110469742+luanhrrr@users.noreply.github.com>
2024-06-14 17:40:25 +0800

    Update CrackedCardData.cs

--- Assets/Scripts/jzc'sTest/jzcCard/CardData/CrackedCardData.cs
+++ Assets/Scripts/jzc'sTest/jzcCard/CardData/CrackedCardData.cs
@@ -7,16 +7,4 @@ public class CrackedCardData : ScriptableObject
 {
     public CardPieceData[] card_pieces = new CardPieceData[4];
 
-    public CrackedCardData DeepCopy()
-    {
-        CrackedCardData copiedCard = ScriptableObject.CreateInstance<CrackedCardData>();
-        copiedCard.card_pieces = new CardPieceData[card_pieces.Length];
-
-        for (int i = 0; i < card_pieces.Length; i++)
-        {
-            copiedCard.card_pieces[i] = card_pieces[i].DeepCopy();
-        }
-
-        return copiedCard;
-    }
 }
