4198293be53c73d8fd2b3893c73cac7f1da79d73
unknown <luanhrr01@gmail.com>
2024-07-15 10:47:01 +0800

    final version July 15thver1.1

--- Assets/Scripts/Guide/Ga.cs
+++ Assets/Scripts/Guide/Ga.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Ga : MonoBehaviour
 {
     private const string SetFCalledKey = "SetFCalled"; // 与 Guide1.cs 中相同的键
+    private const string SetFCalledKey1 = "SetFCalled1";
 
     void Awake()
     {
         // 在游戏启动时重置 SetFCalledKey 为 0
+        PlayerPrefs.SetInt(SetFCalledKey, 0);
         PlayerPrefs.SetInt(SetFCalledKey, 0);
         PlayerPrefs.Save();
     }
--- Assets/Scripts/Guide/Guide4.cs
+++ Assets/Scripts/Guide/Guide4.cs
@@ -6,9 +6,32 @@ public class Guide4 : MonoBehaviour
 {
     public GameObject g1;
     public GameObject g2;
+    private const string SetFCalledKey1 = "SetFCalled1"; // 用于标记 SetF 是否被调用过的键
+
+    void Start()
+    {
+        // 检查 SetF 是否已被调用过
+        if (PlayerPrefs.GetInt(SetFCalledKey1, 0) == 0)
+        {
+            //SetF();
+        }
+        else
+        {
+            Debug.Log("SetFCalledKey set to: " + PlayerPrefs.GetInt(SetFCalledKey1));
+            g1.SetActive(false);
+        }
+    }
+
     public void SetF()
     {
         g1.SetActive(false);
         g2.SetActive(true);
+
+        // 标记 SetF 为已调用
+        PlayerPrefs.SetInt(SetFCalledKey1, 1);
+        PlayerPrefs.Save(); // 确保更改被保存
+
+        // 调试输出
+        Debug.Log("SetFCalledKey set to: " + PlayerPrefs.GetInt(SetFCalledKey1));
     }
 }
